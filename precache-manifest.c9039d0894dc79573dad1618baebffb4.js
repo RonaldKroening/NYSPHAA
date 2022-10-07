c56eb0bde660ddd0cfe69388e443fb77c79b46d3
Ronald Kroening <rk24279n@pace.edu>
2022-10-07 14:47:27 -0400

    Updates

---
+++ precache-manifest.c9039d0894dc79573dad1618baebffb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e3437efb8c0f78018f306e643aaf55",
+    "url": "/NYSPHAA/index.html"
+  },
+  {
+    "revision": "edcbcf497a2e93d6423e",
+    "url": "/NYSPHAA/static/css/main.44375fd2.chunk.css"
+  },
+  {
+    "revision": "e7dd52db502d91cc7372",
+    "url": "/NYSPHAA/static/js/2.09ef5e16.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/NYSPHAA/static/js/2.09ef5e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edcbcf497a2e93d6423e",
+    "url": "/NYSPHAA/static/js/main.0ae68f76.chunk.js"
+  },
+  {
+    "revision": "164c45f4d52eb6f484ab",
+    "url": "/NYSPHAA/static/js/runtime-main.e81485c6.js"
+  },
+  {
+    "revision": "9c2b719c77f30d27fecb90d1383d03db",
+    "url": "/NYSPHAA/static/media/Person.9c2b719c.png"
+  }
+]);
